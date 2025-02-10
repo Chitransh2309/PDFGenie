fa0616d33cec584a8c66cd6d795e91c457fd9755
Chitransh2309 <geniussaxena007@gmail.com>
2025-02-10 13:35:55 +0530

    config

--- backend/server.js
+++ backend/server.js
@@ -26,3 +26,7 @@ mongoose
 // Start server
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
+app.get("/", (req, res) => {
+  res.send("Backend is running successfully!");
+});
