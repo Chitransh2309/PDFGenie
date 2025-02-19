786e7c15e9ce4ae0a72fcc3b8b0f383877595ed8
Chitransh2309 <geniussaxena007@gmail.com>
2025-02-20 00:11:09 +0530

    config

--- src/components/About.js
+++ src/components/About.js
@@ -8,7 +8,7 @@ export class About extends Component {
         <div className="container">
           <div className="box">
             <div>
-              <p className='my-1 mx-3'>The AIQ+ is an intelligent system designed to automatically clean, validate, and enrich datasets by leveraging advanced machine learning algorithms. It detects and corrects inconsistencies, removes duplicates, fills in missing values, and identifies anomalies in real time, ensuring high-quality, reliable data for analytics and decision-making. The system continuously learns from patterns to improve data integrity, enabling businesses to maintain accurate records and optimize processes. With seamless integration into existing data pipelines, it enhances operational efficiency while reducing manual effort, making it an essential tool <br/>for organizations handling large volumes of data.</p>
+              <p className='my-1 mx-3'>The AIQ+ is an intelligent system designed to automatically clean, validate, and enrich datasets by leveraging advanced machine learning algorithms. It detects and corrects inconsistencies, removes duplicates, fills in missing values, and identifies anomalies in real time, ensuring high-quality, reliable data for analytics and decision-making. The system continuously learns from patterns to improve data integrity, enabling businesses to maintain accurate records and optimize processes. With seamless integration into existing data pipelines, it enhances operational efficiency while reducing manual effort, making it an essential tool for organizations handling large volumes of data.</p>
             </div>
           </div>
         </div>
