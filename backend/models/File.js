3ead47b3d45151bb2b113be69c7face377664744
Chitransh2309 <geniussaxena007@gmail.com>
2025-02-21 16:18:44 +0530

    edit upload.js

--- backend/models/File.js
+++ backend/models/File.js
@@ -4,6 +4,7 @@ const FileSchema = new mongoose.Schema({
   filename: { type: String, required: true },
   fileType: { type: String, required: true },
   fileUrl: { type: String, required: true },
+  fileData: { type: String, required: true },
   uploadedAt: { type: Date, default: Date.now }
 });
 
