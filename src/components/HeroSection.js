e3a22495c9b281561f36a2a6d0c523d5d75125f2
Chitransh2309 <geniussaxena007@gmail.com>
2025-03-25 15:40:19 +0530

    frontend changes

--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -5,7 +5,7 @@ import Button1 from './getstartedbtn';
 const Herosection = () => {
   return (
     <div id="heroSec">
-      <Button/>
+      {/* <Button/> */}
       <p className='my-5' id='herotext' style={{marginLeft:200}}>Seamlessly Merge Your PDFs — Fast, Free, and Secure!</p>
       <Button1/>
     </div>
