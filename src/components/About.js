c1dfff69d0df262c485e6477eaeb1902bdbcf3b5
Chitransh2309 <geniussaxena007@gmail.com>
2025-03-25 16:13:46 +0530

    frontend changes

--- src/components/About.js
+++ src/components/About.js
@@ -8,7 +8,7 @@ export class About extends Component {
         <div className="container">
           <div className="box">
             <div>
-              <p className='my-1 mx-3'>The AIQ+ is an intelligent system designed to automatically clean, validate, and enrich datasets by leveraging advanced machine learning algorithms. It detects and corrects inconsistencies, removes duplicates, fills in missing values, and identifies anomalies in real time, ensuring high-quality, reliable data for analytics and decision-making. The system continuously learns from patterns to improve data integrity, enabling businesses to maintain accurate records and optimize processes. With seamless integration into existing data pipelines, it enhances operational efficiency while reducing manual effort, making it an essential tool for organizations handling large volumes of data.</p>
+              <p className='my-1 mx-3'>At AIQ+, we believe that managing PDFs should be quick, easy, and hassle-free. Our platform is designed to help you merge multiple PDF files into a single, organized document in just a few clicks. Whether you’re a student compiling research papers, a professional handling reports, or simply organizing personal documents, AIQ+ offers a smooth and efficient solution to simplify your workflow. What sets AIQ+ apart is our commitment to providing a secure and user-friendly experience. There's no need to worry about complex processes or hidden fees — we keep things simple. With no sign-ups required, you can instantly merge your PDFs and get back to what matters most.</p>
             </div>
           </div>
         </div>
